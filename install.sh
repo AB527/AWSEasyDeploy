b37d46d02ea33d8825325f8d0038ac6cc793f548
Atharva Bedekar <atharvabedekar527@gmail.com>
2026-03-02 12:22:26 +0530

    fix: correct version in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 REPO="AB527/AWSEasyDeploy"
@@ -9,15 +8,30 @@ BINARY="easy-deploy"
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-if [ "$ARCH" = "x86_64" ]; then
-  ARCH="amd64"
+[ "$ARCH" = "x86_64" ] && ARCH="amd64"
+
+echo "Fetching latest version..."
+
+VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest \
+  | grep tag_name \
+  | cut -d '"' -f4)
+
+FILE="${PROJECT}_${VERSION}_${OS}_${ARCH}.tar.gz"
+URL="https://github.com/$REPO/releases/download/${VERSION}/${FILE}"
+
+echo "Installing $PROJECT $VERSION..."
+echo "Downloading: $FILE"
+
+TMP=$(mktemp)
+
+if ! curl -fL "$URL" -o "$TMP"; then
+  echo "❌ Failed to download binary"
+  echo "Expected: $FILE"
+  exit 1
 fi
 
-URL="https://github.com/$REPO/releases/latest/download/${PROJECT}_${OS}_${ARCH}.tar.gz"
-
-echo "Installing $PROJECT..."
-
-curl -sL $URL | tar xz
+tar -xzf "$TMP"
+rm "$TMP"
 
 chmod +x $BINARY
 sudo mv $BINARY /usr/local/bin/
